4f646ce829dddcb8dc95d1abb00fc5b54d6039f7
saidoud <develop.softoud@gmail.com>
2021-10-19 16:40:04 +0100

    Sign in

--- src/components/Form/SignIn.jsx
+++ src/components/Form/SignIn.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
 import {
   Avatar,
   Button,
@@ -11,6 +13,11 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import { Box } from "@mui/system";
 
 function SignIn() {
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().required().email().label("email"),
+    password: Yup.string().required().min(4).label("Password"),
+  });
+
   return (
     <>
       <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
@@ -19,34 +26,62 @@ function SignIn() {
       <Typography component="h1" variant="h5">
         Sign in
       </Typography>
-      <Box sx={{ mt: 1 }}>
-        <TextField
-          margin="normal"
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-        />
-        <TextField
-          margin="normal"
-          fullWidth
-          id="password"
-          label="Password"
-          name="password"
-        />
-        <FormControlLabel
-          control={<Checkbox value="remember" color="primary" />}
-          label="Remember me"
-        />
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          sx={{ mt: 3, mb: 2 }}
-        >
-          Sign In
-        </Button>
-      </Box>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validationSchema={validationSchema}
+        validate={() => console.log("validate")}
+        onSubmit={() => console.log("submit")}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          /* and other goodies */
+        }) => (
+          <Box sx={{ mt: 1 }} component="form" onSubmit={handleSubmit}>
+            <TextField
+              error={touched["email"]}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              helperText={errors["email"]}
+              margin="normal"
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+            />
+            <TextField
+              error={touched["password"]}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              helperText={errors["password"]}
+              id="password"
+              label="Password"
+              name="password"
+              type="password"
+              fullWidth
+              margin="normal"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={() => console.log(touched["email"])}
+            >
+              Sign In
+            </Button>
+          </Box>
+        )}
+      </Formik>
     </>
   );
 }
